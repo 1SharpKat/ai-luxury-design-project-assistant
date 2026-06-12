a2c082178d07d82692f9dc073351382369aedac5
Kat Sharp <sharp.kat76@gmail.com>
2026-06-12 12:49:52 -0600

    Set frontend to demo mode

--- frontend/config.js
+++ frontend/config.js
@@ -1,11 +1,11 @@
 window.LUXNOTE_CONFIG = {
   apiBaseUrl: "https://mqg99s0svc.execute-api.us-west-2.amazonaws.com",
-  authEnabled: true,
-  cognitoDomain: "https://us-west-21z1wwfubv.auth.us-west-2.amazoncognito.com",
-  cognitoClientId: "47j65orcqfdch5ts6vkq3kli2i",
-  cognitoRedirectUri: "https://luxnote.ai/index.html",
-  cognitoLogoutUri: "https://luxnote.ai/index.html",
+  authEnabled: false,
+  cognitoDomain: "",
+  cognitoClientId: "",
+  cognitoRedirectUri: "",
+  cognitoLogoutUri: "",
   cognitoScopes: "openid email profile",
   allowExternalCoverUrls: true,
-  demoNotice: false
+  demoNotice: true
 };
